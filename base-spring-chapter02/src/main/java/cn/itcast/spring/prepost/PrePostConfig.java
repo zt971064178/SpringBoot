e24fabc21e48e0a19632b3c68f2cc38efff30b63
zt971064178 <zhang.tian20080808@163.com>
2017-04-16 20:45:10 +0800

    代码修改

--- base-spring-chapter02/src/main/java/cn/itcast/spring/prepost/PrePostConfig.java
+++ base-spring-chapter02/src/main/java/cn/itcast/spring/prepost/PrePostConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan("cn.itcast.spring.prepost")
+@ComponentScan("cn.itcast.spring.prepost")
 public class PrePostConfig {
 
 	/*
